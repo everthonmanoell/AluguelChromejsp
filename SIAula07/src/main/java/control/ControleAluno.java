22fa81047713534355f62effa32b1b8d23b4763a
everthonmanoell <everthonmanoell@gmail.com>
2023-12-02 21:13:39 -0300

    Criei o 'model' e o 'control' e ajustei os erros, já estão prontos para uso

---
+++ SIAula07/src/main/java/control/ControleAluno.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package control;
+
+import model.*;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author evert
+ */
+public class ControleAluno {
+
+    private LinkedList<Aluno> alunos;
+
+    private static ControleAluno instancia = null;
+
+    private ControleAluno() {
+
+        this.alunos = new LinkedList<Aluno>();
+
+    }
+
+    public static ControleAluno getInstance() {
+        if (instancia == null) {
+            instancia = new ControleAluno();
+        }
+        return instancia;
+    }
+
+    public void adicionarAluno(Aluno aluno) {
+        this.alunos.add(aluno);
+    }
+
+    //------------------ ALUNO -------------------------//
+    //------------------ ALUNO -------------------------//
+    public Aluno getAluno(String id) {
+        for (Aluno c : this.alunos) {
+            if (c.getId().equals(id)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public String getIdAluno(String id) {
+        for (Aluno c : this.alunos) {
+            if (c.getId().equals(id)) {
+                return id;
+            }
+        }
+        return null;
+    }
+
+    public String getMatriculaAluno(String matricula) {
+        for (Aluno c : this.alunos) {
+            if (c.getMatricula().equals(matricula)) {
+                return matricula;
+            }
+        }
+        return null;
+    }
+
+    public void alterarAluno(String id, String nome, String matricula, String turno, String periodo, String turma) {
+        for (Aluno c : this.alunos) {
+            if (String.valueOf(c.getId()).equals(id)) {
+                c.setNome(nome);
+                c.setMatricula(matricula);
+                c.setTurno(turno);
+                c.setPeriodo(periodo);
+                c.setTurma(turma);
+                break; // interrompe o loop após encontrar o Chromebook correspondente
+            }
+        }
+    }
+
+    public String listarDadosAluno() {
+        String texto = "";
+        for (Aluno u : this.alunos) {
+            texto = texto
+                    + "<tr>"
+                    + "<td>" + u.getId() + "</td>"
+                    + "<td>" + u.getNome() + "</td>"
+                    + "<td>" + u.getMatricula() + "</td>"
+                    + "<td>" + u.getTurno() + "</td>"
+                    + "<td>" + u.getPeriodo() + "</td>"
+                    + "<td>" + u.getTurma() + "</td>"
+                    + "<td><a href=\"cadastroaluno.jsp?id=" + u.getId() + "\" class=\"btn btn-outline-primary btn-sm\">Alterar</a>"
+                    + "<a href=\"validar/excluiraluno.jsp?id=" + u.getId() + "\" class=\"btn btn-outline-danger btn-sm\" onclick=\"return confirm('Tem certeza que deseja excluir?')\">Excluir</a>\n"
+                    + "</tr>";
+        }
+        return texto;
+    }
+
+    public void excluirAluno(String id) {
+        for (Aluno c : this.alunos) {
+            if (c.getId().equals(id)) {
+                this.alunos.remove(c);
+
+                break;
+            }
+        }
+
+    }
+
+    public String contarAluno() {
+
+        return this.alunos.size() + "";
+
+    }
+
+    //----------------------------------------------------------------//
+}
---
+++ SIAula07/src/main/java/model/Aluguel.java
@@ -0,0 +1,171 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import usuario.*;
+
+/**
+ *
+ * @author evert
+ */
+public class Aluguel {
+
+
+
+    private String id;
+    private String matriculaUsuario;
+    private String matricula;    
+    private String tombamento;
+    private String situacaoChromebook;
+    private String dataAluguel;
+    private String horaInicio;
+    private String dataTermino;
+    private String horaTermino;
+    
+
+    public Aluguel(String id, String matriculaUsuario, String matricula, String tombamento, String situacaoChromebook, String dataAluguel, String horaInicio, String dataTermino ,String horaTermino ) {
+        
+        this.id = id;
+        this.matriculaUsuario = matriculaUsuario;
+        this.matricula = matricula;
+        this.tombamento = tombamento;
+        this.situacaoChromebook = situacaoChromebook;
+        this.dataAluguel = dataAluguel;
+        this.horaInicio = horaInicio;
+        this.dataTermino = dataTermino;
+        this.horaTermino = horaTermino;
+        
+
+    }
+
+    /*
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /*
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the matricula
+     */
+    public String getMatricula() {
+        return matricula;
+    }
+
+    /*
+     * @param matricula the matricula to set
+     */
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    /*
+     * @return the idMatriculaUsuario
+     */
+    public String getMatriculaUsuario() {
+        return matriculaUsuario;
+    }
+
+    /*
+     * @param idMatriculaUsuario the idMatriculaUsuario to set
+     */
+    public void setMatriculaUsuario(String matriculaUsuario) {
+        this.matriculaUsuario = matriculaUsuario;
+    }
+
+    /*
+     * @return the tombamento
+     */
+    public String getTombamento() {
+        return tombamento;
+    }
+
+    /*
+     * @param tombamento the tombamento to set
+     */
+    public void setTombamento(String tombamento) {
+        this.tombamento = tombamento;
+    }
+
+    /*
+     * @return the situacaoChromebook
+     */
+    public String getSituacaoChromebook() {
+        return situacaoChromebook;
+    }
+
+    /*
+     * @param situacaoChromebook the situacaoChromebook to set
+     */
+    public void setSituacaoChromebook(String situacaoChromebook) {
+        this.situacaoChromebook = situacaoChromebook;
+    }
+
+    /*
+     * @return the dataAluguel
+     */
+    public String getDataAluguel() {
+        return dataAluguel;
+    }
+
+    /*
+     * @param dataAluguel the dataAluguel to set
+     */
+    public void setDataAluguel(String dataAluguel) {
+        this.dataAluguel = dataAluguel;
+    }
+
+    /**
+     * @return the horaInicio
+     */
+    public String getHoraInicio() {
+        return horaInicio;
+    }
+
+    /*
+     * @param horaInicio the horaInicio to set
+     */
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    /*
+     * @return the horaTermino
+     */
+    public String getHoraTermino() {
+        return horaTermino;
+    }
+
+    /*
+     * @param horaTermino the horaTermino to set
+     */
+    public void setHoraTermino(String horaTermino) {
+        this.horaTermino = horaTermino;
+    }
+
+    /*
+     * @return the dataTermino
+     */
+    public String getDataTermino() {
+        return dataTermino;
+    }
+
+    /*
+     * @param dataTermino the dataTermino to set
+     */
+    public void setDataTermino(String dataTermino) {
+        this.dataTermino = dataTermino;
+    }
+
+    
+    
+}
