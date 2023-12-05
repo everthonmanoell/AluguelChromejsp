fe06ad7b41aa013e48dc3f1f8eb0e2fd979d0fe4
Wilson Serafim <76951790+wilsonserafim@users.noreply.github.com>
2023-12-04 22:36:58 -0300

    implementando o getHoraAtual e data

--- SIAula07/src/main/java/control/ControleAluguel.java
+++ SIAula07/src/main/java/control/ControleAluguel.java
@@ -1,9 +1,10 @@
 package control;
 
-
-
+import java.time.LocalTime;
 import model.*;
 import java.util.LinkedList;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Everthon
@@ -46,8 +47,7 @@ public class ControleAluguel {
         }
         return null;
     }
-    
-    
+
     // Método para buscar informações e adicionar ao aluguel
     public void adicionarAluguel(String idAluguel, String matriculaUsuario, String idMatricula, String tombamento,
             String situacaoChromebook, String dataAluguel, String horaInicio, String horaTermino, String dataTermino) {
@@ -57,28 +57,28 @@ public class ControleAluguel {
         String chromebook = this.controleChromebook.getTombamento(tombamento);
 
         //if (usuario.equals(matriculaUsuario) && (aluno.equals(idMatricula)) && (chromebook.equals(tombamento))) {
+        Aluguel novoAluguel = new Aluguel(idAluguel, matriculaUsuario, idMatricula,
+                tombamento, situacaoChromebook, dataAluguel, horaInicio, horaTermino, dataTermino);
 
-            Aluguel novoAluguel = new Aluguel(idAluguel, matriculaUsuario, idMatricula,
-                    tombamento, situacaoChromebook, dataAluguel, horaInicio, horaTermino, dataTermino);
-
-            this.alugueis.add(novoAluguel);
+        this.alugueis.add(novoAluguel);
         /*} else {
             System.out.println("Usuário, aluno ou Chromebook não encontrado.");
         }*/
 
     }
-    
-    public void alterarAluguel (String idAluguel, String situacaoChromebook,String horaTermino, String dataTermino){
-                for (Aluguel c : this.alugueis) {
+
+    public void alterarAluguel(String idAluguel, String situacaoChromebook) {
+        for (Aluguel c : this.alugueis) {
             if (String.valueOf(c.getId()).equals(idAluguel)) {
 
                 c.setSituacaoChromebook(situacaoChromebook);
-                c.setHoraTermino(horaTermino);
+                String getHoraAtual = getHoraAtual;
+                c.setHoraTermino(getHoraAtual);
                 c.setDataTermino(dataTermino);
                 break; // interrompe o loop após encontrar o Chromebook correspondente
             }
         }
-    
+
     }
 
     public String listarDadosAlugueis() {
@@ -99,47 +99,69 @@ public class ControleAluguel {
         }
         return texto;
     }
-    
-    
-        
-        
+
     public int contarAlugueis() {
-            int contador = 0;
-            for (Aluguel u : this.alugueis) {
-                if (u.getDataTermino() == null || u.getDataTermino().isEmpty()) {
-                    contador++;
-                }
+        int contador = 0;
+        for (Aluguel u : this.alugueis) {
+            if (u.getDataTermino() == null || u.getDataTermino().isEmpty()) {
+                contador++;
             }
+        }
         return contador;
     }
-    
-    
-        public String contarAlugados() {
+
+    public String contarAlugados() {
 
         return this.alugueis.size() + "";
 
     }
-        
-        
-        public String listarDadosAlugados() {
+     public String getHoraAtual() {
+        // Obtém a hora atual
+        LocalTime horaAtual = LocalTime.now();
+
+        // Formata a hora atual como uma string (opcional)
+        String horaFormatada = horaAtual.toString();
+
+        // Retorna a hora atual ou a string formatada
+        return horaFormatada;
+    }
+
+    public String getHoraAtualFormatada() {
+        // Obtém a hora atual
+        LocalTime horaAtual = LocalTime.now();
+
+        // Define o formato desejado (por exemplo, HH:mm:ss)
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        // Formata a hora atual conforme o formato
+        String horaFormatada = horaAtual.format(formatter);
+
+        // Retorna a hora formatada
+        return horaFormatada;
+    }
+
+    
+    
+    public String listarDadosAlugados() {
         String texto = "";
         for (Aluguel u : this.alugueis) {
-            
-                texto += "<tr>"
-                        + "<td>" + u.getId() + "</td>"
-                        + "<td>" + u.getMatriculaUsuario() + "</td>"
-                        + "<td>" + u.getMatricula() + "</td>"
-                        + "<td>" + u.getTombamento() + "</td>"
-                        + "<td>" + u.getSituacaoChromebook() + "</td>"
-                        + "<td>" + u.getDataAluguel() + "</td>"
-                        + "<td>" + u.getHoraInicio() + "</td>"
-                        + "<td>" + u.getHoraTermino() + "</td>"
-                        + "<td>" + u.getDataTermino() + "</td>"
-                        + "</tr>";
-            
+
+            texto += "<tr>"
+                    + "<td>" + u.getId() + "</td>"
+                    + "<td>" + u.getMatriculaUsuario() + "</td>"
+                    + "<td>" + u.getMatricula() + "</td>"
+                    + "<td>" + u.getTombamento() + "</td>"
+                    + "<td>" + u.getSituacaoChromebook() + "</td>"
+                    + "<td>" + u.getDataAluguel() + "</td>"
+                    + "<td>" + u.getHoraInicio() + "</td>"
+                    + "<td>" + u.getHoraTermino() + "</td>"
+                    + "<td>" + u.getDataTermino() + "</td>"
+                    + "</tr>";
+
         }
         return texto;
     }
 
 //----------------------------------------------------------------------
+   
 }
